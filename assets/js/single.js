01bfc6809cc800e59704e2ce2b98bebf0697abf5
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-10-20 17:14:40 -0500

    added links to github from discoverd issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -16,7 +16,39 @@ var getRepoIssues = function (repo) {
 };
 
 var displayIssues = function(issues) {
-    
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+
+  for (var i = 0; i < issues.length; i++) {
+    // create a link element to take users to the issue on github
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+    // create span to hold issue title
+var titleEl = document.createElement("span");
+titleEl.textContent = issues[i].title;
+
+// append to container
+issueEl.appendChild(titleEl);
+
+// create a type element
+var typeEl = document.createElement("span");
+
+issueContainerEl.appendChild(issueEl);
+
+// check if issue is an actual issue or a pull request
+if (issues[i].pull_request) {
+  typeEl.textContent = "(Pull request)";
+} else {
+  typeEl.textContent = "(Issue)";
+}
+
+// append to container
+issueEl.appendChild(typeEl);
+  }
 };
 
 getRepoIssues("facebook/react");
